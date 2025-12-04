cc2a28dbaaff391011601cf6d172a93d84246da7
OhMinGeun9609 <oksk199428@gmail.com>
2025-12-04 12:28:56 +0900

    feat :  피격, 사망 사운드 추가

--- Assets/Scripts/Effects/AudioSourceHandler.cs
+++ Assets/Scripts/Effects/AudioSourceHandler.cs
@@ -6,10 +6,25 @@ public class AudioSourceHandler : MonoBehaviour
 {
     [Header("현재 사용중인 무기의 사운드 제어 스크립트")]
     private SoundEffect currentSound;
+    [Header("플레이어 자체의 효과음")]
+    public AudioClip damaged;
+    public AudioClip death;
+    private AudioSource mainSound;
 
     private void Start()
     {
+        mainSound = GetComponent<AudioSource>();
         currentSound = GetComponentInChildren<SoundEffect>();   
+    }
+
+    public void HurtSound()
+    {
+        mainSound.PlayOneShot(damaged);
+    }
+
+    public void DeathSound()
+    {
+        mainSound.PlayOneShot(death);
     }
 
     public void ShootSound()
